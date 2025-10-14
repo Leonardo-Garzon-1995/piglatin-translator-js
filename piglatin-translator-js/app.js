fb837370be73ceebd3c4d0f5ca2b557331430c2a
lgarzonparada1995-netizen <lgarzonparada1995@gmail.com>
2025-10-14 10:09:32 +0200

    Add files via upload

---
+++ piglatin-translator-js/app.js
@@ -0,0 +1,14 @@
+
+function translatePigLatin(str) {
+    const beginWithCons = /^([b-df-hj-np-tv-z]+)(.*)/gi
+    const beginWithVowel = /^[aeiou]+/gi
+
+    if (beginWithVowel.test(str)) {
+        return str + "way"
+    }
+
+    if (beginWithCons.test(str)) {
+        return str.replace(beginWithCons, "")
+    }
+}
+
