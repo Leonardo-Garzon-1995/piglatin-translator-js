0b5371ebcb69690b7ca0f965f776f5176e9aecf7
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-10-15 06:24:39 +0200

    add npm
    add chalk package and script 'start'

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import {translatePigLatin} from "./index.js"
+import chalk from "chalk"
 
-let test = translatePigLatin("Cat")
+let testWord = translatePigLatin("Car")
 
-console.log(test)
+console.log(chalk.yellow(testWord.toUpperCase())) 
