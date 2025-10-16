2dcb4c086781400e5047c7a50131d354d9df01a2
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-10-16 06:00:44 +0200

    add promt to the cli

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import {translatePigLatin} from "./index.js"
 import chalk from "chalk"
+import PromptSync from "prompt-sync"
 
-const randomIndexA = Math.floor(Math.random() * 256)
-const randomIndexB = Math.floor(Math.random() * 256)
-const randomIndexC = Math.floor(Math.random() * 256)
+const prompt = PromptSync()
+
+const inputText = prompt("Enter a word: ")
 
 
-console.log(randomIndexA, randomIndexB, randomIndexC)
 const logTranslation = (str) => {
+    const randomIndexA = Math.floor(Math.random() * 256)
+    const randomIndexB = Math.floor(Math.random() * 256)
+    const randomIndexC = Math.floor(Math.random() * 256)
+
     let text = translatePigLatin(str).toUpperCase()
     let colorChange= chalk.rgb(randomIndexA, randomIndexB, randomIndexC)(text)
     console.log(colorChange)
 }
 
-logTranslation("What")
+logTranslation(inputText)
